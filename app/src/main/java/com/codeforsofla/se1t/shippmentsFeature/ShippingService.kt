78b95df4cd1a6e3be5debba807d49d7c40ab4a25
Brad M. Harris <bmichaelh13@gmail.com>
2022-09-28 22:23:02 -0400

    updated shiping details for testing vendors

--- app/src/main/java/com/codeforsofla/se1t/shippmentsFeature/ShippingService.kt
+++ app/src/main/java/com/codeforsofla/se1t/shippmentsFeature/ShippingService.kt
@@ -16,17 +16,17 @@ class MockShippingService : ShippingService {
     override fun getShipments(): List<ShippingData> {
         return listOf(
             ShippingData(1,"Todd", "Amazon", "11:00AM - 12:00PM"),
-            ShippingData(2,"Brad", "Mikes Fishes", "12:00PM - 1:00PM"),
+            ShippingData(2,"Brad", "Lancer Deets Testing", "12:30PM - 1:00PM"),
             ShippingData(3,"Matthew", "Publix Foods", "2:00PM - 3:00PM"),
-            ShippingData(3,"Matthew", "Publix Foods", "2:00PM - 3:00PM"),
-            ShippingData(3,"Matthew", "Publix Foods", "2:00PM - 3:00PM"),
-            ShippingData(3,"Matthew", "Publix Foods", "2:00PM - 3:00PM"),
-            ShippingData(3,"Matthew", "Publix Foods", "2:00PM - 3:00PM"),
-            ShippingData(3,"Matthew", "Publix Foods", "2:00PM - 3:00PM"),
-            ShippingData(3,"Matthew", "Publix Foods", "2:00PM - 3:00PM"),
-            ShippingData(3,"Matthew", "Publix Foods", "2:00PM - 3:00PM"),
-            ShippingData(3,"Matthew", "Publix Foods", "2:00PM - 3:00PM"),
-            ShippingData(3,"Matthew", "Publix Foods", "2:00PM - 3:00PM"),
+            ShippingData(3,"Matthew", "Publix Foods", "4:00PM - 5:00PM"),
+            ShippingData(3,"Matthew", "Publix Foods", "6:00PM - 7:00PM"),
+            ShippingData(3,"Matthew", "Publix Foods", "8:00PM - 9:00PM"),
+            ShippingData(3,"Matthew", "Publix Foods", "10:00PM - 11:00PM"),
+            ShippingData(3,"Matthew", "Publix Foods", "1:00AM - 2:00AM"),
+            ShippingData(3,"Matthew", "Publix Foods", "3:00AM - 4:00AM"),
+            ShippingData(3,"Matthew", "Publix Foods", "5:00AM - 6:00AM"),
+            ShippingData(3,"Matthew", "Publix Foods", "7:00AM - 8:00AM"),
+            ShippingData(3,"Matthew", "Publix Foods", "9:00AM - 10:00AM"),
         )
     }
 
